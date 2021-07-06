84a9e6c99ecded14f79d7fc71c0e367f9e531c02
freeburma <kophyoblue@gmail.com>
2021-07-06 15:26:14 +1200

    Moved Header&Footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+  <!-- Footer -->
+  <div class="container-fluid bg-primary footer-container">
+        <div class="row">
+            <div class="col footer-copyright">
+                <span>
+                    Copyright: 2021. All rights reserved. 
+                </span>
+            </div>
+        </div>
+    </div>
+        
+
+
+
+</body>
+</html>
