b574e8139c0adad65cabfa326287f54be2c257a6
freeburma <kophyoblue@gmail.com>
2021-07-15 20:34:18 +1200

    Loaded the data from db

--- index.php
+++ index.php
@@ -7,115 +7,37 @@
         <!-- Cards -->
         <div class="row">
 
-            <!-- card 1-->
-            <div class="col-md-4">
-                    <div class="card" >
-                        <img class="card-img-top" src="<?php echo get_template_directory_uri() . '/images/IMG_4707.JPG' ?>" alt="Card image" style="width:100%">
-                        <div class="card-body">
-                            <h4 class="card-title">McLaren</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                            <a href="#" class="btn btn-primary">Detail</a>
-                        </div>
-                    </div><!--End card -->
-            </div> <!-- End Col-->
+            <?php 
+                global $wpdb; 
 
-            <!-- card 2-->
-            <div class="col-md-4">
-                <div class="card" >
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_4708.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Mini Cooper</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
+                $productsFromDbQuery = $wpdb->prepare(" SELECT * FROM wp_custom_inventory"); 
 
-            <!-- card 3-->
-            <div class="col-md-4">
-                <div class="card" >
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_4717.JPG' ?>"  alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Lamborghini</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
+                $productsFromDb = $wpdb->get_results($productsFromDbQuery); 
 
-            <!-- card 4-->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_4919.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Covertte</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
+                
+            ?>
+            <!-- cards-->
+            <?php 
+                foreach ($productsFromDb as $prod)
+                {
 
-            <!-- card 5-->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_5537.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Holden Corolado</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
+            ?>
+                    <div class="col-md-4">
+                            <div class="card" >
+                                <img class="card-img-top" src="<?php echo get_template_directory_uri() . "/" . $prod->FilePath . "/" . $prod->ImageName_1; ?>" alt="Card image" style="width:100%">
+                                <div class="card-body">
+                                    <h4 class="card-title"> <?php echo $prod-> Title; ?> </h4>
+                                    <p class="card-text"><?php echo $prod->ProductDescription; ?></p>
+                                    <a href="#" class="btn btn-primary">Detail</a>
+                                </div>
+                            </div><!--End card -->
+                    </div> <!-- End Col-->
+            <?php 
+                }// end foreach
 
-            <!-- card 6 -->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_5555.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Range Rover Discovery</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
+            ?>
 
-            <!-- card 7-->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_4972.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Monster Track</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
-
-            <!-- card 8-->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_4714.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Ducati</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
-
-            <!-- card 9-->
-            <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="<?php echo get_theme_file_uri() . './images/IMG_5511.JPG' ?>" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">Yamaha YZF R3</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod vero doloremque quas? Dolores sed explicabo sapiente a alias impedit.</p>
-                        <a href="#" class="btn btn-primary">Detail</a>
-                    </div>
-                </div><!--End card -->
-            </div> <!-- End Col-->
-
-
+            
         </div><!-- End row -->
     </div><!-- End Container -->
 
